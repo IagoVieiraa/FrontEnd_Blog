7e96224eb3536bdc9de18276b310f1faae14a1b8
IagoVieiraa <iago.vieiras13@gmail.com>
2022-02-14 23:40:22 -0300

    Front Blog Pessoal

---
+++ blot-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1>Home</h1>
+            <img src="https://sep-bucket-prod.s3.amazonaws.com/wp-content/uploads/2021/11/desk-III-768x432.jpg" alt="capa palmeiras" />
+        </>
+    );
+}
+
+export default Home;
+
