e1e16e3717101272b7bdb37c94af81ad81b1db57
IagoVieiraa <iago.vieiras13@gmail.com>
2022-02-17 21:58:59 -0300

    refatoração de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Navbar from './componentes/estatico/navbar/Navbar'
 import Footer from './componentes/estatico/footer/Footer'
 import './App.css';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login'
 
 function App() {
   return (
-    <>
-     <Navbar />
-       <Home />
-     <Footer />
-    </>
+    <Router>
+      <Navbar />
+      <Switch>
+        <div style={{minHeight: '100vh'}}>
+          <Route exact path='/'>
+            <Login />
+          </Route>
+
+          <Route path='/login'>
+            <Login />
+          </Route>
+
+          <Route path='/home'>
+            <Home />
+          </Route>
+        </div>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
