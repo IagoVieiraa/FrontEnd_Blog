07a5a1d27b5355589d3b4655d4839aa7bc74560e
IagoVieiraa <iago.vieiras13@gmail.com>
2022-03-04 15:59:37 -0300

    Adição Redux

--- src/componentes/estatico/footer/Footer.tsx
+++ src/componentes/estatico/footer/Footer.tsx
@@ -4,40 +4,52 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import './Footer.css';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
 
 function Footer() {
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    var footerComponent;
+
+    if (token != '') {
+        footerComponent = <Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid alignItems="center" item xs={12}>
+                <Box className='box1'>
+                    <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                        <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" justifyContent="center">
+                        <a href="https://www.facebook.com" target="blank">
+                            <FacebookIcon className='redes' />
+                        </a>
+                        <a href="https://www.instagram.com" target="blank">
+                            <InstagramIcon className='redes' />
+                        </a>
+                        <a href="https://www.linkedin.com/in/iago-vieira-499076165/" target="blank">
+                            <LinkedInIcon className='redes' />
+                        </a>
+                    </Box>
+                </Box>
+                <Box className='box2'>
+                    <Box paddingTop={1}>
+                        <Typography variant="subtitle2" align="center" gutterBottom className='textos1' >© 2022 Copyright:</Typography>
+                    </Box>
+                    <Box>
+                        <a target="blank" href="https://brasil.generation.org">
+                            <Typography variant="subtitle2" gutterBottom className='textos1' align="center">brasil.generation.org</Typography>
+                        </a>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+    }
+
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box className='box1'>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com" target="blank">
-                                <FacebookIcon className='redes' />
-                            </a>
-                            <a href="https://www.instagram.com" target="blank">
-                                <InstagramIcon className='redes' />
-                            </a>
-                            <a href="https://www.linkedin.com/in/iago-vieira-499076165/" target="blank">
-                                <LinkedInIcon className='redes' />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box className='box2'>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom className='textos1' >© 2022 Copyright:</Typography>
-                        </Box>
-                        <Box>
-                            <a target="blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom className='textos1' align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
+            {footerComponent}
         </>
     )
 }
--- src/componentes/estatico/navbar/Navbar.tsx
+++ src/componentes/estatico/navbar/Navbar.tsx
@@ -2,73 +2,86 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 import { Link, useHistory } from 'react-router-dom';
 import './Navbar.css';
-import useLocalStorage from 'react-use-localstorage';
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { addToken } from '../../../store/tokens/actions';
 
 function Navbar() {
     
-    const [token, setToken] = useLocalStorage('token');
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
     let history = useHistory();
+    const dispatch = useDispatch();
+    
     //função para limpar o token quando se deslogar
     function goLogout() {
-        setToken('')
+        dispatch(addToken(''));
         alert("Usuário deslogado")
         history.push('/login')
     }
+
+    var navbarComponent;
+
+    if (token != '') {
+        navbarComponent = <AppBar position="static">
+            <Toolbar variant="dense" style={{ backgroundColor: "#224a17" }}>
+                <Link to='/home' className='text-decorator-none'>
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal
+                        </Typography>
+                    </Box>
+                </Link>
+
+                <Box display="flex" justifyContent="start">
+                    <Link to='/home' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                home
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to='/postagens' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                postagens
+                            </Typography>
+                        </Box>
+                    </Link>
+
+                    <Link to='/temas' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                temas
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to='/formularioTema' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to='/login' className='text-decorator-none'>
+                        {/*onClick - vai chamar a função goLogout */}
+                        <Box mx={1} className='cursor' onClick={goLogout}>
+                            <Typography variant="h6" color="inherit">
+                                Logout
+                            </Typography>
+                        </Box>
+                    </Link>
+
+                </Box>
+
+            </Toolbar>
+        </AppBar>
+    }
+
         return (
             <>
-                <AppBar position="static">
-                    <Toolbar variant="dense" style={{ backgroundColor: "#224a17" }}>
-                        <Link to='/home' className='text-decorator-none'>
-                            <Box className='cursor'>
-                                <Typography variant="h5" color="inherit">
-                                    BlogPessoal
-                                </Typography>
-                            </Box>
-                        </Link>
-
-                        <Box display="flex" justifyContent="start">
-                            <Link to='/home' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        home
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/postagens' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        postagens
-                                    </Typography>
-                                </Box>
-                            </Link>
-
-                            <Link to='/temas' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        temas
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/formularioTema' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h6" color="inherit">
-                                        cadastrar tema
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/login' className='text-decorator-none'>
-                                {/*onClick - vai chamar a função goLogout */}
-                                <Box mx={1} className='cursor' onClick={goLogout}>
-                                    <Typography variant="h6" color="inherit">
-                                        Logout
-                                    </Typography>
-                                </Box>
-                            </Link>
-                        
-                        </Box>
-
-                    </Toolbar>
-                </AppBar>
+                {navbarComponent}
             </>
         )
     }
